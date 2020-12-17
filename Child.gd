06713cf47e0c29ce32a175b0a59b9e8f2a0690ca
The maintainer <wxns@protonmail.ch>
2020-12-17 19:36:53 +0000

    Added .gitattributes for line ending normalization

---
+++ Child.gd
@@ -0,0 +1,37 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var parentName
+var p1
+var p2
+var p3
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#parentName = get_parent().get_parent().name
+	
+	# terminal style relative node reference
+	parentName = get_node("./../..").name
+	print(self.get_node("./..").get_child_count())
+	
+	# go from top
+	parentName = get_tree().get_root().get_node("TheRoot").get_child(0).get_child(0).name
+	
+	print("My grandparent is " + parentName)
+	
+	# attempt to infer types
+	p1 = get_tree()
+	p2 = get_tree().get_root()
+	p3 = get_tree().get_root().get_node("TheRoot")
+	
+	print(str(typeof(get_tree())) + str(p1)) # SceneTree
+	print(str(typeof(get_tree().get_root())) + str(p2)) # Viewport
+	print(str(typeof(get_tree().get_root().get_node("TheRoot"))) + str(p3)) #node
+	print(typeof(parentName)) # string
+	
+	print(find_node("Child2",true,false))
+	print(get_node("./..").get_children()[1].name + " found!")
---
+++ GrandParent.gd
@@ -0,0 +1,21 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print(get_child(0).get_child(1).name)
+	print(find_node("Child", true, false))
+	pass # Replace with function body.
+
+	# switching between scenes
+	#get_tree().change_scene("MyScene2.tscn")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
