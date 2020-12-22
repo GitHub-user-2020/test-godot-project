c70f0d2fd9bdd5b670f54ac9c4f5ccf8906dfa9d
The Maintainer <wxns@protonmail.ch>
2020-12-22 17:39:52 +0000

    Added variables to SceneOne script

--- GrandParent.gd
+++ GrandParent.gd
@@ -13,7 +13,7 @@ func _ready():
 	pass # Replace with function body.
 
 	# switching between scenes
-	print("Changing scene to \"SceneOne.tscn\"...")
+	print("Changing the current scene to \"SceneOne.tscn\"...")
 	get_tree().change_scene("SceneOne.tscn")
 
 
--- SceneOne.gd
+++ SceneOne.gd
@@ -4,14 +4,22 @@ extends Node
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var a = null
+var myArray = Array()
+# Called when tree loaded in - called in after parent's own _enter_tree()
+func _enter_tree():
+	print("SceneOne has been loaded successfully.")
 
-
-# Called when the node enters the scene tree for the first time.
+# Called when the node enters the scene tree for the first time. Called after _enter_tree(); only called when children ready.
 func _ready():
-	print("Hello world!")
+	for i in range(0,10,1):
+		myArray.append(i)
+		print(10-i)
 	print(self.get_node(".").name)
 	pass # Replace with function body.
 
+#func _input():
+#	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
